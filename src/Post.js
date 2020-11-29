370f4e2b36b2f46ce4839dbbe696377fc4ddf268
seolleung2 <seolleung22@gmail.com>
2020-11-29 14:14:25 +0900

    Add buttons on Post component, apply hover over effect CSS

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
-import { TodayTwoTone } from "@material-ui/icons";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlinedIcon from "@material-ui/icons/ChatBubbleOutlined";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 function Post({ profilePic, image, username, timestamp, message }) {
   return (
@@ -18,6 +22,24 @@ function Post({ profilePic, image, username, timestamp, message }) {
       </div>
       <div className="post__image">
         <img src={image} alt="" />
+      </div>
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutlinedIcon />
+          <p>Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreOutlinedIcon />
+        </div>
       </div>
     </div>
   );
