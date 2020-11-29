d52403a0b203a11bfa7e984cb447dd7a33c3495d
seolleung2 <seolleung22@gmail.com>
2020-11-29 21:08:22 +0900

    Can UPLOAD image, message ON FEED using firebase DB

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -6,6 +6,8 @@ import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 import { useStateValue } from "./StateProvider";
+import db from "./firebase";
+import firebase from "firebase";
 
 function MessageSender() {
   const [{ user }, dispatch] = useStateValue();
@@ -14,7 +16,13 @@ function MessageSender() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // some clever db stuff
+    db.collection("posts").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      profilePic: user.photoURL,
+      username: user.displayName,
+      image: imageUrl,
+    });
 
     setInput("");
     setImageUrl("");
